3dc1fc7c6501f67e91b458711fc54c795190fe08
Supremo1337 <lucaswyllame12@gmail.com>
2022-11-26 12:45:00 -0300

    fix(OurTeam): Resposivity

--- src/components/OurTeam/styles.js
+++ src/components/OurTeam/styles.js
@@ -119,34 +119,50 @@ export const NameCasters = styled.label`
 export const FuncCasters = styled.label`
   font-size: 20px;
   color: #9a8dac;
-  @media (min-width: 1024px) {
-  }
 `;
 
 export const LeftArrow = styled.img`
   height: 39px;
   width: 39px;
-  position: absolute;
-  top: 50%;
-  left: -3vw;
+  position: relative;
+  top: 130vh;
+  left: 50px;
   z-index: 2;
-  @media (max-width: 480px) {
-    position: relative;
-    top: 139vh;
-    left: 13vw;
+  @media (min-width: 769px) {
+    top: 48.5vh;
+    left: 257px;
+  }
+  @media (min-width: 1024px) {
+    position: absolute;
+    top: 50%;
+    left: -6vw;
+  }
+  @media (min-width: 1176px) {
+    position: absolute;
+    left: -52px;
+    top: 50%;
   }
 `;
 
 export const RightArrow = styled.img`
   height: 39px;
   width: 39px;
-  position: absolute;
-  left: 100%;
-  top: 50%;
+  position: relative;
+  top: 59px;
+  left: 160px;
   z-index: 2;
-  @media (max-width: 480px) {
-    position: relative;
-    top: 10.6vw;
-    left: 39vw;
+  @media (min-width: 769px) {
+    top: 25px;
+    left: 350px;
+  }
+  @media (min-width: 1024px) {
+    position: absolute;
+    left: 680px;
+    top: 50%;
+  }
+  @media (min-width: 1176px) {
+    position: absolute;
+    left: 959px;
+    top: 50%;
   }
 `;
