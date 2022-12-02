2999baf66737269e52d9792c1695b226ef1b6d1f
Supremo1337 <lucaswyllame12@gmail.com>
2022-12-02 10:40:43 -0300

    fix(AvaiablePlats): contentful implementing

--- src/components/AvailablePlats/index.js
+++ src/components/AvailablePlats/index.js
@@ -13,27 +13,22 @@ import { useState, useEffect } from "react";
 import { ConnectContent } from "src/ConfigContent";
 
 export default function AvailablePlats() {
-  const [cardsInfos, setCardsInfos] = useState([]);
-  const [buttonsName, setButtonsName] = useState([]);
+  const [all, setAll] = useState([]);
   var teste;
   useEffect(() => {
     async function FetchMyApi() {
       let items = await ConnectContent();
-      let cardsInfosContent = await items.filter(
+      let allContent = await items.filter(
         (x) => x.sys.contentType.sys.id == "availablePlats"
       );
-      let buttonsNameContent = await items.filter(
-        (x) => x.sys.contentType.sys.id == "availablePlatsButtons"
-      );
-      setCardsInfos(cardsInfosContent);
-      setButtonsName(buttonsNameContent);
+      setAll(allContent);
     }
     FetchMyApi();
   }, []);
 
   return (
     <Content>
-      {console.log("La ele", cardsInfos)}
+      {console.log("La ele", all)}
       <Title textAlign="center" margin="0 0 22px 0">
         Plataformas disponíveis
       </Title>
@@ -41,10 +36,10 @@ export default function AvailablePlats() {
         Onde encontrar nosso podcast para escutar
       </Desc>
       <Plats>
-        {cardsInfos.map((res, index) => {
+        {all.slice(0, 3).map((res, index) => {
           return (
             // eslint-disable-next-line react/jsx-key
-            <PlatsGroup colorHoverText={res.colorHoverText} key={index}>
+            <PlatsGroup colorHoverText={res.fields.styling?.colorHoverText} key={index}>
               <PlatsCards
                 width={res.fields.styling?.width}
                 height={res.fields.styling?.height}
@@ -52,51 +47,44 @@ export default function AvailablePlats() {
                 heightHover={res.fields.styling?.heightHover}
                 widthHoverDiv={res.fields.styling?.widthHoverDiv}
                 heightHoverDiv={res.fields.styling?.heightHoverDiv}
-                href={res.fields.styling?.url}
-                imageHover={`url(${res.fields.platsIconsHover?.fields.file.url})` || ""}
+                href={res.fields.url}
+                imageHover={
+                  `url(${res.fields.iconHover?.fields.file.url})` || ""
+                }
                 target="_blank"
                 rel="noreferrer"
               >
                 <PlatsIcons
                   widthIcon={res.fields.styling?.widthIcon}
                   heightIcon={res.fields.styling?.heightIcon}
-                  image={`url(${res.fields.platsIcons?.fields.file.url})` || ""}
+                  image={`url(${res.fields.icon?.fields.file.url})` || ""}
                 />
               </PlatsCards>
               <PlatsTitle
-                href={res.url}
+                href={res.fields.url}
                 target="_blank"
                 rel="noreferrer"
                 fontSize={res.fields.styling?.fontSize}
                 fontWeight={res.fields.styling?.fontWeight}
                 margin={"15px 0 0 0"}
               >
-                {res.fields.platsName}
+                {res.fields.platformName}
               </PlatsTitle>
             </PlatsGroup>
           );
         })}
       </Plats>
-      {/* {buttonsName.map((res, index) => {
-        return ( */}
+      {all.slice(3).map((res, index) => {
+        return (
           <PlatsButton
             href="https://bit.ly/3Qe9421"
             target="_blank"
             rel="noreferrer"
           >
-            {/* {res.fields.buttonName} */}
-            Podcast Addict
+            {res.fields.platformName}
           </PlatsButton>
-          <PlatsButton
-            href="https://bit.ly/3Qe9421"
-            target="_blank"
-            rel="noreferrer"
-          >
-            {/* {res.fields.buttonName} */}
-            Sugerir plataforma
-          </PlatsButton>
-        {/* );
-      })} */}
+        );
+      })}
     </Content>
   );
 }
