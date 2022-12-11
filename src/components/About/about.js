1241a967be8f159e5e091bf7929e49804f23262d
Supremo1337 <lucaswyllame12@gmail.com>
2022-12-11 12:57:32 -0300

    feature(About): Apresentation contentful implement

--- src/components/About/about.js
+++ src/components/About/about.js
@@ -1,19 +1,37 @@
 import { Desc, MoreInfs, Title } from "@styles/globalStyles";
+import { useState, useEffect } from "react";
+import { ConnectContent } from "src/ConfigContent";
 import { Content, LeftImage, TextDiv } from "./styles";
 
 export default function About() {
+  const [all, setAll] = useState([]);
+
+  useEffect(() => {
+    async function FetchMyApi() {
+      let items = await ConnectContent();
+      let allContent = await items.filter(
+        (x) => x.sys.contentType.sys.id == "apresentation"
+      );
+      setAll(allContent);
+    }
+    FetchMyApi();
+  }, []);
   return (
     <Content>
-      <LeftImage/>
+      {console.log("About(apresentation) all", all)}
+      <LeftImage />
       <TextDiv>
         <Title margin="0 0 5px 0">Somos o Hyper Beam</Title>
-        <Desc margin="0 0 22px 0"> A fagulha de amor aos games em forma de podcast.</Desc>
-        <MoreInfs fontSize={"1.4rem"}>
-          Lorem ipsum dolor sit amet, consectetur a elit. Sed dictum sodales
-          rutrum. Praesent eget lobortis purus. Donec a finibus neque. Nu erat a
-          cursus sodales. Fusce vel nulla aliquam, convallis odio semper,
-          finibus purus. orci, non malesuada augue.
-        </MoreInfs>
+        <Desc margin="0 0 22px 0">
+          A fagulha de amor aos games em forma de podcast.
+        </Desc>
+        {all.map((res, index) => {
+          return (
+            <MoreInfs key={index} fontSize={"1.4rem"}>
+              {res.fields.apresentationText}
+            </MoreInfs>
+          );
+        })}
       </TextDiv>
     </Content>
   );
