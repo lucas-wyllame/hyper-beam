815695dd5cc8435383f451b23155fd89e79315eb
Supremo1337 <lucaswyllame12@gmail.com>
2023-01-31 10:38:11 -0300

    fix(Commets): Remove disqus and add new style to this component

--- src/components/Comments/index.js
+++ src/components/Comments/index.js
@@ -1,50 +1,61 @@
-import Disqus from "disqus-react";
 import styled from "styled-components";
 import { useState, useEffect } from "react";
-const envUrl = require('../../envUrl.json')
+import { PlatsCards } from "../AvailablePlats/styles";
+import { Desc, MoreInfs, Title } from "@styles/globalStyles";
+import { EmailPokeCenter } from "@icon/EmailPokeCenter";
+import { BrTag, Divider, GroupTexts } from "./styles";
 
 const Content = styled.div`
   width: 100%;
-  height: 800px;
-  background: #27273d;
+  height: 305px;
+  /* background: #27273d; */
   display: flex;
+  flex-direction: column;
   align-items: center;
-  justify-content: center;
-  margin-top: 54px;
-`;
-
-const DisqusDiv = styled.div`
-  width: 90%;
+  justify-content: space-between;
+  margin: 40px 0 100px 0;
+  @media (min-width: 768px) {
+    height: 362px;
+    margin: 80px 0 110px 0;
+  }
+  @media (min-width: 1024px) {
+    height: 324px;
+    margin: 85px 0 120px 0;
+  }
 `;
 
 export default function Comments() {
-  const [disqusUrl, setDisquisUrl] = useState("");
-  const [disqusId, setDisquisId] = useState("");
-
-  useEffect(() => {
-    setDisquisId(
-      window.location.href.toString().replace(`${envUrl.link}/podcasts/`, "")
-    );
-    setDisquisUrl(window.location.href.toString());
-  }, []);
-
-  const disqusShortname = "hyper-beam";
-  const disqusConfig = {
-    url: disqusUrl,
-    identifier: disqusId,
-    title: disqusId,
-  };
   return (
     <Content>
-      <DisqusDiv>
-        {disqusUrl && (
-          <Disqus.DiscussionEmbed
-            shortname={disqusShortname}
-            config={disqusConfig}
-            theme="auto"
-          />
-        )}
-      </DisqusDiv>
+      <GroupTexts>
+        <Title textAlign="center" margin="0">
+          O que achou do episódio?
+        </Title>
+        <Desc textAlignDesc="center" margin="0">
+          Mande um e-mail e apareça no PokéCenter, nossa leitura de e-mails no
+          fim de cada episódio
+        </Desc>
+      </GroupTexts>
+      <PlatsCards
+        width="346px"
+        height="111px"
+        widthTablet="557px"
+        widthDesktop="682px"
+        heightDesktop="126px"
+      >
+        <EmailPokeCenter />
+        <Divider />
+        <MoreInfs
+          fontSize="1.4rem"
+          fontSizeTablet="1.9rem"
+          fontSizeLaptop="2.3rem"
+          fontSizeLaptopLarge="2.3rem"
+        >
+          Mande um e-mail pra gente!
+          <br />
+          <span>Apareça na leitura de e-mails</span>
+        </MoreInfs>
+      </PlatsCards>
     </Content>
   );
 }
