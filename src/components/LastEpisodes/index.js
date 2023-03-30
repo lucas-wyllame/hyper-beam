f084d941e608c5cfb131adccf8624a20b8350e92
Supremo1337 <lucaswyllame12@gmail.com>
2023-03-30 18:32:43 -0300

    enable autoplay

--- src/components/LastEpisodes/index.js
+++ src/components/LastEpisodes/index.js
@@ -114,7 +114,7 @@ export default function LastEpisodes() {
     slidesToScroll: 1,
     nextArrow: <SampleNextArrow />,
     prevArrow: <SamplePrevArrow />,
-    // autoplay: true,
+    autoplay: true,
     autoplaySpeed: 4000,
     responsive: [
       {
