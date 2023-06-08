d37f5c24240d11e1ca31bcb3ef75427407e6051b
Supremo1337 <lucaswyllame12@gmail.com>
2023-06-08 17:36:11 -0300

    add icon and title

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,10 +4,23 @@ import LastEpisodes from "src/components/LastEpisodes";
 import OurTeam from "src/components/OurTeam";
 import AvailablePlats from "src/components/AvailablePlats";
 import { GlobalStyles } from "@styles/globalStyles";
+import Head from "next/head";
 
 export default function Home() {
   return (
     <>
+      <div>
+        <Head>
+          <title>Hyper Beam Podcast</title>
+          <meta
+            name="viewport"
+            content="initial-scale=1.0, width=device-width"
+          />
+        </Head>
+
+        <p>Hello world!</p>
+      </div>
+
       <HomeBanner />
       <About />
       <GlobalStyles />
