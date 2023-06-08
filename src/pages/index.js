716554774dd2cdff37ef1354fb2eb9c518da5e95
Supremo1337 <lucaswyllame12@gmail.com>
2023-06-08 17:51:42 -0300

    add meta descritption

--- src/pages/index.js
+++ src/pages/index.js
@@ -16,6 +16,7 @@ export default function Home() {
             name="viewport"
             content="initial-scale=1.0, width=device-width"
           />
+          <meta name='description' content='A fagulha de amor aos games em forma de podcast.' />
         </Head>
 
         <p>Hello world!</p>
